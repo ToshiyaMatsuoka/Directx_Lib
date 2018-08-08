7639ce6ad19e5ae570b2c5a8c9d0002942787f59
ToshiyaMatsuoka <triger.1306@gmail.com>
2018-08-08 12:56:47 +0900

    関数の説明を追加

--- DirectX_LIB/DirectX_LIB/TrialEnums.h
+++ DirectX_LIB/DirectX_LIB/TrialEnums.h
@@ -1,12 +1,20 @@
 #ifndef ENUM_H
 #define ENUM_H
+
+/**
+* @enum TEXTURE
+* テクスチャ配列の配列番号
+*/
 enum TEXTURE
 {
 	HOGE_TEX,
 	BOO_TEX,
 	TEXMAX
 };
-
+/**
+* @enum FONT
+* フォント配列の配列番号
+*/
 enum FONT {
 	FONT,
 	FONTMAX
--- DirectX_LIB/DirectX_LIB/TrialLib.h
+++ DirectX_LIB/DirectX_LIB/TrialLib.h
@@ -1,3 +1,12 @@
+/**
+* @file TrialLib.h
+* @brief DirectX9及びXInputのライブラリ
+* @author Toshiya Matsuoka
+* @date 2018/7/7
+*/
+
+
+
 #ifndef TRIALLIB_H
 #define TRIALLIB_H
 
@@ -94,51 +103,250 @@ extern BYTE KeyOldState[256];
 
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//DirecrtXInitialize
+
+/**
+* @brief DirecrtXのInitialize
+* @param hWnd　ウィンドウハンドル
+* @param pSrcFile　画像ファイル名
+* @return 成功すればS_OK、失敗でE_FAIL
+*/
 HRESULT InitD3d(HWND hWnd, LPCSTR pSrcFile);
+/**
+* @brief フルスクリーン用DirecrtXのInitialize
+* @param hWnd　ウィンドウハンドル
+* @param pSrcFile　画像ファイル名
+* @return 成功すればS_OK、失敗でE_FAIL
+*/
 HRESULT InitD3dFullscreen(HWND hWnd, LPCSTR pSrcFile, int ResolutionWidth, int ResolutionHeight);
+/**
+* @brief DirecrtInputのInitialize
+* @param hWnd　ウィンドウハンドル
+* @return 成功すればS_OK、失敗でE_FAIL
+*/
 HRESULT InitDinput(HWND hWnd);
+/**
+* @brief DirecrtXDeviceのInitialize
+* @param hWnd　ウィンドウハンドル
+* @param pSrcFile　画像ファイル名
+* @return 成功すればS_OK、失敗でE_FAIL
+*/
 HRESULT InitDirectX(HWND hWnd, LPCSTR pSrcFile);
+/**
+* @brief フルスクリーン用DirecrtXDeviceのInitialize
+* @param hWnd　ウィンドウハンドル
+* @param pSrcFile　画像ファイル名
+* @return 成功すればS_OK、失敗でE_FAIL
+*/
 HRESULT InitDirectXFullscreen(HWND hWnd, LPCSTR pSrcFile, int ResolutionWidth, int ResolutionHeight);
 
 void FreeDx();
 
 //WINAPIの空ウィンドウ生成
+
+/**
+* @brief ウィンドウ生成
+* @param WndName ウィンドウタイトル及びクラスの名称
+* @param WIDTH 画面幅
+* @param HEIGHT 画面高さ
+* @param hInst APIインスタンスのハンドル
+* @param hInstance APIインスタンスのハンドル
+* @param  IconIDI　IconのID　不要ならNULL
+* @param pSrcFile　画像ファイル名
+* @return NULL返すとウィンドウを閉じる
+*/
 int InitWindow(LPCSTR WndName, int WIDTH, int HEIGHT, HINSTANCE hInst, HINSTANCE hInstance, int IconIDI, LPCSTR pSrcFile);
+
+/**
+* @brief ウィンドウ生成
+* @param WndName ウィンドウタイトル及びクラスの名称
+* @param hWnd　ウィンドウハンドル
+* @param WIDTH 画面幅
+* @param HEIGHT 画面高さ
+* @param hInst APIインスタンスのハンドル
+* @param hInstance APIインスタンスのハンドル
+* @param  IconIDI　IconのID　不要ならNULL
+* @param pSrcFile　画像ファイル名
+* @return NULL返すとウィンドウを閉じる
+*/
 int InitWindowEx(LPCSTR WndName, HWND* hWnd, int WIDTH, int HEIGHT, HINSTANCE hInst, HINSTANCE hInstance, int IconIDI, LPCSTR pSrcFile);
+
+/**
+* @brief フルスクリーン用ウィンドウ生成
+* @param WndName ウィンドウタイトル及びクラスの名称
+* @param hWnd　ウィンドウハンドル
+* @param WIDTH 画面幅
+* @param HEIGHT 画面高さ
+* @param hInst APIインスタンスのハンドル
+* @param hInstance APIインスタンスのハンドル
+* @param  IconIDI　IconのID　不要ならNULL
+* @param pSrcFile　画像ファイル名
+* @return NULL返すとウィンドウを閉じる
+*/
 int InitWindowFullscreenEx(LPCSTR WndName, HWND* hWnd, int WIDTH, int HEIGHT, HINSTANCE hInst, HINSTANCE hInstance, int IconIDI, LPCSTR pSrcFile);
 
+/**
+* @brief 秒間60フレームループさせる
+* @param gameroop(void) 自分の作成したゲーム処理関数を入れること
+* @return プログラム終了時に(int)msg.wParam
+*/
 int FlameRoop(void gameroop(void));
 
 //描画
+
+/**
+* @brief 描画開始関数
+* @sa EndSetTexture()
+* @detail 上記関数とセットで使うこと
+*/
 void BeginSetTexture();
+/**
+* @brief 描画終了関数
+* @sa BeginSetTexture()
+* @detail 上記関数とセットで使うこと
+*/
 void EndSetTexture();
 
-//画像読み込み
+
+/**
+* @brief 画像読み込み
+* @param pTextureName 読み込む画像ファイル名
+* @param TexNum 画像の格納先配列番号
+*/
 void ReadInTexture(LPCSTR pTextureName, int TexNum);
 
-//画像表示
+
+/**
+* @brief 画像表示
+* @param Vertex 描画する4頂点情報
+* @param TexNum 画像の格納配列番号
+* @detail CUSTOMVERTEX変数は自分で用意すること
+*/
 void SetUpTexture(CUSTOMVERTEX* Vertex, int TexNum);
 
 //2頂点設定描画
+
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Left 左端
+* @param Top 上端
+* @param Right 右端
+* @param Bottom 下端
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateSquareVertex(float Left, float Top, float Right, float Bottom, int TexNum);
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Left 左端
+* @param Top 上端
+* @param Right 右端
+* @param Bottom 下端
+* @param color 色
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateSquareVertexColor(float Left, float Top, float Right, float Bottom, DWORD color, int TexNum);
+
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Left 左端
+* @param Top 上端
+* @param Right 右端
+* @param Bottom 下端
+* @param color 色
+* @param tu 画像切り取り左端
+* @param tv 画像切り取り上端
+* @param scaleTu 画像の切り取り幅
+* @param scaleTv 画像の切り取り高さ
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateSquareVertexEx( float Left, float Top, float Right, float Bottom, DWORD  color, float tu, float tv, float scaleTu, float scaleTv, int TexNum);
 
 //RECT引数2頂点設定描画
+
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Vertex RECT構造体で頂点を指定する
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateRECTVertex(RECT Vertex, int TexNum);
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Vertex RECT構造体で頂点を指定する
+* @param color 色
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateRECTVertexColor(RECT Vertex, DWORD color, int TexNum);
+/**
+* @brief 2頂点を指定し画像を描画する
+* @param Vertex RECT構造体で頂点を指定する
+* @param color 色
+* @param tu 画像切り取り左端
+* @param tv 画像切り取り上端
+* @param scaleTu 画像の切り取り幅
+* @param scaleTv 画像の切り取り高さ
+* @param TexNum 画像の格納配列番号
+*/
 void EasyCreateRECTVertexEx(RECT Vertex, DWORD color, float tu, float tv, float scaleTu, float scaleTv, int TexNum);
 
 //4頂点設定
+
+/**
+* @brief CUSTOMVERTEX変数に値を入れる
+* @param Vertex 入れ物
+* @param Central 入れる中心情報
+*/
 void CreateSquareVertex(CUSTOMVERTEX* Vertex, CENTRAL_STATE Central);
+/**
+* @brief CUSTOMVERTEX変数に値を入れる
+* @param Vertex 入れ物
+* @param Central 入れる中心情報
+* @param color 色
+* @param tu 画像切り取り左端
+* @param tv 画像切り取り上端
+* @param scaleTu 画像の切り取り幅
+* @param scaleTv 画像の切り取り高さ
+*/
 void CreateSquareVertexEx(CUSTOMVERTEX* Vertex, CENTRAL_STATE Central, float tu, float tv, float scaleTu, float scaleTv);
+/**
+* @brief CUSTOMVERTEX変数に値を入れる
+* @param Vertex 入れ物
+* @param Central 入れる中心情報
+* @param color 色
+* @param tu 画像切り取り左端
+* @param tv 画像切り取り上端
+* @param scaleTu 画像の切り取り幅
+* @param scaleTv 画像の切り取り高さ
+*/
 void CreateSquareVertexColorEx(CUSTOMVERTEX* Vertex, CENTRAL_STATE Central, DWORD  color, float tu, float tv, float scaleTu, float scaleTv);
+/**
+* @brief CUSTOMVERTEX変数に値を入れる
+* @param Vertex 入れ物
+* @param Central 入れる中心情報
+* @param color 色
+*/
 void CreateSquareVertexColor(CUSTOMVERTEX* Vertex, CENTRAL_STATE Central, DWORD  color);
 
+
 //DXフォント
-//文字設定
+
+/**
+* @brief 文字設定
+* @param WordHeight 文字の高さ
+* @param WordWidth 文字の幅
+* @param CharSet キャラセット（英字なら１，シフトJISなら128）
+* @param FontName 使用するフォント名
+* @param FontNum 表示文字設定の格納先配列番号
+*/
 void SetUpFont(int WordHeight, int WordWidth, int CharSet, LPCSTR FontName, int FontNum);
-//描画設定
+/**
+* @brief 描画設定
+* @param Texts 表示内容
+* @param Vertex 表示範囲
+* @param TextFormat フォーマット
+* @param color 色
+* @param FontNum 表示文字設定の格納配列番号
+*/
 void WriteWord(LPCSTR Texts, RECT Vertex, int TextFormat, int color, int FontNum);
 
 //回転、円運動関数
